75a910da72e5106e2c275c13fc2b1cc6a8d050c1
lxdi <alexfordeal@gmail.com>
2019-03-15 14:56:39 +0500

    UI refactoring

--- frontend/source/app.js
+++ frontend/source/app.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import Modal from 'react-modal';
-import {Frame} from './ui/frame';
-import {SwitchButton} from './ui/changebutton';
-import {state} from './state';
-import {Button} from 'react-bootstrap'
 
+import {Main} from './ui/main'
 import {FinishingTaskModal} from './ui/means/finishing-task-modal'
 
 import './data/realms-dao'
@@ -21,68 +17,10 @@ ReactDOM.render(<div id="app" />, document.body);
 const app = document.getElementById("app");
 
 function rerender(){
-	ReactDOM.render(<Main />, app);
-}
-
-class Main extends React.Component {
-	constructor(props){
-		super(props);
-		this.state = {}
-		this.switchMode = this.switchMode.bind(this);
-	}
-
-	switchMode(){
-		state.threeFrames = !state.threeFrames
-		this.setState({})
-	}
-
-	render() {
-		var leftFrame = null;
-		var rightFrame = null;
-		var thirdFrame = null;
-		var framesTable = null;
-		var switchButton = <div></div>;
-		if(!state.threeFrames){
-			if(state.currState == "Targets"){
-				leftFrame = <Frame name="Targets" />
-				rightFrame = <Frame name="Means" />
-			} else {
-				leftFrame = <Frame name="Means" />
-				rightFrame = <Frame name="Schedule" />
-			}
-			switchButton = <div class='switch-button-div'>
-												{<SwitchButton rerender={rerender} />}
-										</div>
-			framesTable = <table class='frames-table'>
-								<tr>
-									<td class='frame-td'>{leftFrame}</td>
-									<td class='frame-td'>{rightFrame}</td>
-								</tr>
-							</table>
-		} else {
-			leftFrame = <Frame name="Targets" />
-			rightFrame = <Frame name="Means" />
-			thirdFrame = <Frame name="Schedule" />
-			framesTable = <table class='frames-table'>
-									<tr>
-										<td class='frame-td-3f'>{leftFrame}</td>
-										<td class='frame-td-3f'>{rightFrame}</td>
-										<td class='frame-td-3f'>{thirdFrame}</td>
-									</tr>
-								</table>
-		}
-		return (
-			<div>
+	ReactDOM.render(
+		<div>
 				<FinishingTaskModal/>
-				<div align = "center" >
-					{switchButton}
-					<div align='right' class='mode-button-div'>
-						<Button onClick = {this.switchMode}>Mode</Button>
-					</div>
-				</div>
-				{framesTable}
-			</div>
-		)
-	}
+				<Main />
+		</div>, app);
 }
 rerender();
---
+++ frontend/source/ui/main.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Button} from 'react-bootstrap'
+
+import {registerEvent, registerReaction, fireEvent, viewStateVal, registerObject} from '../controllers/eventor'
+
+import {Frame} from './frame';
+//import {SwitchButton} from './changebutton';
+
+export class Main extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {}
+
+    registerObject('main-ui', {'three-frames':true, currState:'Targets'})
+
+    registerEvent('main-ui', 'switch-mode', (stateSetter)=>{
+      stateSetter('three-frames', !viewStateVal('main-ui', 'three-frames'))
+      this.setState({})
+    })
+
+    registerEvent('main-ui', 'switch-curr-state', (stateSetter)=>{
+      if(viewStateVal('main-ui', 'currState')=='Targets'){
+        stateSetter('currState', 'Schedule')
+      } else {
+        stateSetter('currState', 'Targets')
+      }
+      this.setState({})
+    })
+	}
+
+	render() {
+		return (
+			<div>
+				<div align = "center" >
+					{getSwitchButton()}
+					<div align='right' class='mode-button-div'>
+						<Button onClick = {()=>fireEvent('main-ui', 'switch-mode')}>Mode</Button>
+					</div>
+				</div>
+				{getFramesTable()}
+			</div>
+		)
+	}
+}
+
+const getFramesTable = function(){
+  if(!viewStateVal('main-ui', 'three-frames')){
+    var leftFrame = null
+    var rightFrame = null
+    if(viewStateVal('main-ui', 'currState') == "Targets"){
+      leftFrame = <Frame name="Targets" />
+      rightFrame = <Frame name="Means" />
+    } else {
+      leftFrame = <Frame name="Means" />
+      rightFrame = <Frame name="Schedule" />
+    }
+    // switchButton = <div class='switch-button-div'>
+    //                   {<SwitchButton rerender={rerender} />}
+    //               </div>
+    return <table class='frames-table'>
+              <tr>
+                <td class='frame-td'>{leftFrame}</td>
+                <td class='frame-td'>{rightFrame}</td>
+              </tr>
+            </table>
+  } else {
+    return <table class='frames-table'>
+                <tr>
+                  <td class='frame-td-3f'><Frame name="Targets" /></td>
+                  <td class='frame-td-3f'><Frame name="Means" /></td>
+                  <td class='frame-td-3f'><Frame name="Schedule" /></td>
+                </tr>
+              </table>
+  }
+}
+
+const getSwitchButton = function(){
+  if(!viewStateVal('main-ui', 'three-frames')){
+    return <div class='switch-button-div'>
+                      <Button bsStyle="primary" onClick={()=>fireEvent('main-ui', 'switch-curr-state')}>Switch</Button>
+                  </div>
+  } else {
+    return <div></div>
+  }
+}
