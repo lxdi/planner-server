4bb5775ef7f9a5053f5f201626bfa353e38f58a6
lxdi <alexfordeal@gmail.com>
2020-05-16 16:01:05 +0300

    removed useless scripts

--- deploy/properties.py
+++
@@ -1,9 +0,0 @@
-import os
-#Required
-tomcatPath = os.path.join('C:\\', 'Users', 'Alexander', 'Projects', 'tomcats', 'apache-tomcat-7.0.91')
-
-#Optional
-projectRoot = None
-warName = 'ROOT.war'
-runScript = 'startup.bat'
-stopScript = 'shutdown.bat'
