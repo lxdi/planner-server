6a778cfba216ebd7ebfde28cfc9d1d7d37e74c93
lxdi <alexfordeal@gmail.com>
2022-02-20 22:27:49 +0300

    Priorities modal -> tabs

--- frontend/source/ui/means/modal/priorities-modal.js
+++ frontend/source/ui/means/modal/priorities-modal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Table} from 'react-bootstrap'
+import {Table, Tabs, Tab} from 'react-bootstrap'
 import {registerEvent, registerReaction, fireEvent, chkSt} from 'absevents'
 import {CommonModal} from './../../common/common-modal'
 
@@ -37,25 +37,60 @@ const getConent = function(comp){
     return 'Loading...'
   }
 
-  const result = []
+  const realms = chkSt('realm-rep', 'objects')
+
+  const realmIdToMeans = {}
 
   means.forEach(mean => {
-
-    if(mean.layers == null){
-      return
+    if(comp.state.currentRealmId == null){
+      comp.state.currentRealmId = mean.realmid
     }
 
-    mean.layers
-      .filter(l => l.priority>0)
-      .sort((l1, l2) => l1.priority - l2.priority)
-      .forEach(layer => {
+    if(realmIdToMeans[mean.realmid]==null){
+      realmIdToMeans[mean.realmid] = []
+    }
+
+    realmIdToMeans[mean.realmid].push(mean)
+  })
+
+  const tabs = []
+
+  for (const [k, v] of Object.entries(realmIdToMeans)) {
+    tabs.push(<Tab eventKey={k} title={realms[k].title}>{meansUI(v)}</Tab>)
+  }
+
+  return <div>
+            <Tabs activeKey={comp.state.currentRealmId} onSelect={(e)=>handleSelectTab(comp, e)}>
+                {tabs}
+              </Tabs>
+        </div>
+
+}
+
+const handleSelectTab = function(comp, e){
+  comp.setState({currentRealmId: e})
+}
+
+const meansUI = function(means) {
+
+  const result = []
+  var layers = []
+
+  means
+    .filter(mean => mean.layers != null)
+    .forEach(mean => layers = layers.concat(mean.layers))
+
+  layers
+    .filter(l => l.priority>0)
+    .sort((l1, l2) => l1.priority - l2.priority)
+    .forEach(layer => {
         result.push( <tr id={layer.id}>
-                      <td>{mean.title}</td>
+                      <td>{means.filter(m => m.id == layer.meanid)[0].title}</td>
                       <td>Layer {layer.depth}</td>
                       <td>{layer.priority}</td>
                     </tr>)
-    })
-  })
+                    })
+
 
   return  <Table striped bordered condensed hover >
             <tbody>
@@ -67,5 +102,4 @@ const getConent = function(comp){
               {result}
             </tbody>
             </Table>
-
 }
